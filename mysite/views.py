56be58d0e5b8da52d59022aee8862932adac55b8
Albayram <ya5127@cps-c00525.zyxel.com>
2018-11-09 02:57:43 -0500

    second commit

--- mysite/views.py
+++ mysite/views.py
@@ -21,14 +21,14 @@ def test2(request):
     p = People(first_name = "John", last_name = "Doe", age = 22)
     p.save()
 
-    # Get the first row in the database and assign to a variable called u
+    # Get the first row in the database and assign it to a variable called u
     u = People.objects.first()
-    # Print the values in html directly
+    # Print the values
     return HttpResponse("firstname = %s, lastname = %s, age=%s" %(u.first_name, u.last_name, u.age))
 
 def test3(request):
 
-    # Get the first row in the database and assign to a variable called u
+    # Get the first row in the database and assign it to a variable called u
     u = People.objects.first()
 
     # Pass the object u to test2.html in which the fields of u (e.g., first_name, last_name and age) can be displayed
@@ -36,23 +36,24 @@ def test3(request):
     template = 'test2.html'
     return render(request, template, context)
 
+
+def index(request):
+    # get all the records in the people table in db, and then reverse order them so that the last record appears first
+    allPeople = People.objects.order_by('-id')
+
+    # send allPeople object to the index.html where the fields of u (first_name, last_name and age) can be displayed
+    context = {'people': allPeople}
+    template = 'index.html'
+    return render(request, template, context)
+
 def viewRegister(request):
     # View the register page
     return render(request, 'register.html')
 
 
-def index(request):
-    # get the all people in the db and reverse order so that the last record appears first
-    allPeople = People.objects.order_by('-id')
-
-    # send allPeople object to the html where the fields of u (e.g., first_name, last_name and age) can be displayed
-    context = {'people': allPeople}
-    template = 'index.html'
-    return render(request, template, context)
-
 def addUser(request):
 
-    # First check whether the form was sent through a post method
+    # First check if the form has been sent by a post method
     # if so, then get the values
     if request.method == 'POST':
 
@@ -60,14 +61,15 @@ def addUser(request):
         lastName = request.POST.get("lastName")
         age = request.POST.get("age")
 
-        # Add a record to the database
+        # Add a new record into the database
         p = People(first_name = firstName, last_name = lastName, age = age)
         p.save()
-
+        # Redirect to the index.html
         return redirect('index')
     else:
         return HttpResponse("Something went wrong!")
 
 def deleteUser(request):
+    # Delete the first record in the db and then redirect to the index.html
     People.objects.first().delete()
     return redirect('index')
